178dc2dafc7e16bbdaf7ebf0b1efeb65ff942b8c
KyuNam <xmfpes@naver.com>
2017-10-03 22:17:44 +0900

    Spring Boot init

---
+++ src/main/java/com/kyunam/SpringEditorUploadApplication.java
@@ -0,0 +1,12 @@
+package com.kyunam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEditorUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEditorUploadApplication.class, args);
+	}
+}
